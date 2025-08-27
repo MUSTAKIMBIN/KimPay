592bb1a84312d2d163f5e9dc1060c59f06c088b6
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-08-27 16:40:58 +0600

    created home page navigate functionality

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const logInBtn = document
-  .getElementById("logIn")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-    console.log("log in ");
-  });
+document.getElementById("logIn").addEventListener("click", function (e) {
+  e.preventDefault();
+  const mobileNuber = 1234567890;
+  const pinNuber = 1234;
+  const fromMobileNuber = document.getElementById("mobile-number").value;
+  const fromPinNuber = document.getElementById("pin-number").value;
+  const conFromNum = parseInt(fromMobileNuber);
+  const conPinNum = parseInt(fromPinNuber);
+  if (mobileNuber === conFromNum && pinNuber === conPinNum) {
+    window.location.href = "./home.html";
+  } else {
+    alert(`your pin and mobile number was incorrect `);
+  }
+});
