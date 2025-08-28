3d0edc74041e920378f5a44e1602c1a1e85dbbbe
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-08-28 14:47:47 +0600

    complited cash out feature

--- home.js
+++ home.js
@@ -8,3 +8,23 @@ document.getElementById("addMoney").addEventListener("click", function (e) {
   document.getElementById("total-balance").innerText = grandTotalMoney;
   //   console.log(grandTotalMoney);
 });
+
+// togolling feature
+
+document.getElementById("add-money-div").addEventListener("click", function () {
+  document.getElementById("cashOut-from").style.display = "none";
+  document.getElementById("add-money-form").style.display = "block";
+});
+document.getElementById("cashOut-div").addEventListener("click", function () {
+  document.getElementById("cashOut-from").style.display = "block";
+  document.getElementById("add-money-form").style.display = "none";
+});
+
+// cash out feature
+document.getElementById("cashOut").addEventListener("click", function (e) {
+  e.preventDefault();
+  const amountToOut = parseInt(document.getElementById("cashOutAmount").value);
+  let totalMoney = parseInt(document.getElementById("total-balance").innerText);
+  const grandMoney = totalMoney - amountToOut;
+  document.getElementById("total-balance").innerText = grandMoney;
+});
